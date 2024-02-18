30992af525ae0469250aae7d0f879ddcd332cff8
IweZix <IweZix@users.noreply.github.com>
2024-02-18 17:30:16 +0100

    F01.3 stateless widget peaufiné

--- ex1/lib/laureate.dart
+++ ex1/lib/laureate.dart
@@ -13,24 +13,42 @@ class Laureate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: SizedBox(
             width: 500,
-            child: Column(
-              children: [
-                Text(
-                  motivation,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+            child: Card(
+              color:Colors.grey[300],
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      motivation,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  "$firstname $lastname",
-                  textAlign: TextAlign.justify,
-                )
-              ],
-            )));
+                  const SizedBox(height: 20),
+                  Card(
+                    color: Colors.black,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "$firstname $lastname",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+        )
+    );
   }
 }

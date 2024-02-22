c25f052d2b3c246238ddd9189f16cc61426049f9
IweZix <IweZix@users.noreply.github.com>
2024-02-22 10:06:20 +0100

    T02.3 Liste de contacts

--- tuto2/lib/Contact.dart
+++ tuto2/lib/Contact.dart
@@ -10,3 +10,22 @@ class Contact {
   });
 
 }
+
+List<Contact> _createContacts() {
+  final contacts1 = {
+    "Pierre": "0123456789",
+    "Paul": "0123456789",
+    "Jean": "0123456789",
+  }.entries.map((c) => Contact(name: c.key, phone: c.value)).toList();
+
+  final contacts2 = [
+    for (var i = 0; i < 1000; i++)
+      Contact(name: "Contact ${i + 1}", phone: "$i", isFavorite: true),
+  ];
+
+  final contacts3 = List.generate(10, (i) => Contact(name: "C$i", phone: "$i"));
+
+  return [...contacts1, ...contacts2, ...contacts3];
+}
+
+final defaultContacts = _createContacts();
